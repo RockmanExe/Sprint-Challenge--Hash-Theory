255b186d1214f0fd983692be5fff97d18a89e924
unknown <may.alex43@gmail.com>
2018-10-19 14:48:11 -0400

    ex1 and ex2

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -1,5 +1,22 @@
 def get_indices_of_item_weights(weights, limit):
-  pass 
+  # item_weights={}
+  # difference= limit - weights
+  # for i in range(len(weights)):
+  #   if (difference) in item_weights:
+  #     return (i, item_weights[difference])
+  #   else:
+  #     item_weights[weights[i]]= i
+
+  weights_dict={}
+  for i in range(len(weights)):
+    weight = weights[i]
+    if (limit - weight) in weights_dict:
+      return (i, weights_dict[limit- weight])
+    else:
+      weights_dict[weights]= i
+  return ()
+
+get_indices_of_item_weights([4, 6, 10, 15, 16], 21 )
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -1,5 +1,17 @@
 def reconstruct_trip(tickets):
-  pass 
+  ticket_dict={}
+  route = ['']* (len(tickets)-1)
+  for ticket in tickets:
+    ticket_dict[ticket[0]]= ticket[1]
+    if ticket[0] is None:
+      route[0]= ticket[1]
+  for i in range(1, len(route)):
+    if route[i-1] in ticket_dict:
+      route[i] = ticket_dict[route[i-1]]
+    else:
+      return []
+  return route
+
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
